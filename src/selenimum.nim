27a990be45a4488495c81248ee63effa67aafbdf
みゃみゅ玉子 <myamyu@gmail.com>
2021-09-01 16:39:38 +0000

    追加するメソッドをTODOで書いた

--- src/selenimum.nim
+++ src/selenimum.nim
@@ -105,20 +105,274 @@ proc delete(session: SeleniumSession, path: string): JsonNode =
     raise newException(SeleniumProtocolException, "failed to delete request with session.")
 
 #[
-  navigate to url
+  TODO: get timeouts
+  https://w3c.github.io/webdriver/#dfn-get-timeouts
+]#
+
+#[
+  TODO: set timeouts
+  https://w3c.github.io/webdriver/#dfn-set-timeouts
+]#
+
+#[
+  navigate to url(by string)
+  https://w3c.github.io/webdriver/#dfn-navigate-to
 ]#
 proc navigateTo*(session: SeleniumSession, url: string) =
   discard session.post("/url", %*{"url": url})
 
 #[
-  navigate to url
+  navigate to url(by Uri)
+  https://w3c.github.io/webdriver/#dfn-navigate-to
 ]#
 proc navigateTo*(session: SeleniumSession, url: Uri) =
   discard session.post("/url", %*{"url": $url})
 
 #[
-  Get Current Url
+  get current Url
+  https://w3c.github.io/webdriver/#dfn-get-current-url
 ]#
 proc getCurrentUrl*(session: SeleniumSession): Uri =
   let resp = session.get("/url")
   return resp{"value"}.getStr().parseUri()
+
+#[
+  TODO: back
+  https://w3c.github.io/webdriver/#dfn-back
+]#
+
+#[
+  TODO: forward
+  https://w3c.github.io/webdriver/#dfn-forward
+]#
+
+#[
+  TODO: refresh
+  https://w3c.github.io/webdriver/#dfn-refresh
+]#
+
+#[
+  TODO: get title
+  https://w3c.github.io/webdriver/#dfn-get-title
+]#
+
+#[
+  TODO: get window handle
+  https://w3c.github.io/webdriver/#dfn-get-window-handle
+]#
+
+#[
+  TODO: close window
+  https://w3c.github.io/webdriver/#dfn-close-window
+]#
+
+#[
+  TODO: switch to window
+  https://w3c.github.io/webdriver/#dfn-switch-to-window
+]#
+
+#[
+  TODO: get window handles
+  https://w3c.github.io/webdriver/#dfn-get-window-handles
+]#
+
+#[
+  TODO: new window
+  https://w3c.github.io/webdriver/#dfn-new-window
+]#
+
+#[
+  TODO: switch to frame
+  https://w3c.github.io/webdriver/#dfn-switch-to-frame
+]#
+
+#[
+  TODO: switch to parent frame
+  https://w3c.github.io/webdriver/#dfn-switch-to-parent-frame
+]#
+
+#[
+  TODO: get window rect
+  https://w3c.github.io/webdriver/#dfn-get-window-rect
+]#
+
+#[
+  TODO: set window rect
+  https://w3c.github.io/webdriver/#dfn-set-window-rect
+]#
+
+#[
+  TODO: maximize window
+  https://w3c.github.io/webdriver/#dfn-maximize-window
+]#
+
+#[
+  TODO: minimize window
+  https://w3c.github.io/webdriver/#dfn-minimize-window
+]#
+
+#[
+  TODO: fullscreen window
+  https://w3c.github.io/webdriver/#dfn-fullscreen-window
+]#
+
+#[
+  TODO: get active element
+  https://w3c.github.io/webdriver/#dfn-get-active-element
+]#
+
+#[
+  TODO: get element shadow root
+  https://w3c.github.io/webdriver/#dfn-get-element-shadow-root
+]#
+
+#[
+  TODO: find element
+  https://w3c.github.io/webdriver/#dfn-find-element
+]#
+
+#[
+  TODO: find elements
+  https://w3c.github.io/webdriver/#dfn-find-elements
+]#
+
+#[
+  TODO: find element from element
+  https://w3c.github.io/webdriver/#dfn-find-element-from-element
+]#
+
+#[
+  TODO: find elements from element
+  https://w3c.github.io/webdriver/#dfn-find-elements-from-element
+]#
+
+#[
+  TODO: find element from shadow root
+  https://w3c.github.io/webdriver/#dfn-find-element-from-shadow-root
+]#
+
+#[
+  TODO: find elements from shadow root
+  https://w3c.github.io/webdriver/#dfn-find-elements-from-shadow-root
+]#
+
+#[
+  TODO: is element selected
+  https://w3c.github.io/webdriver/#dfn-is-element-selected
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-attribute
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-property
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-css-value
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-text
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-tag-name
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-element-rect
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-is-element-enabled
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-computed-role
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-computed-label
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-element-click
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-element-clear
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-element-send-keys
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-page-source
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-execute-script
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-execute-async-script
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-all-cookies
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-named-cookie
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-adding-a-cookie
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-delete-cookie
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-delete-all-cookies
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-perform-actions
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-release-actions
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-dismiss-alert
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-accept-alert
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-get-alert-text
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-send-alert-text
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-take-screenshot
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-take-element-screenshot
+]#
+
+#[
+  TODO: https://w3c.github.io/webdriver/#dfn-print-page
+]#
