fc138bb09cc6eeab61d2decd1ef1f1ca03737d52
みゃみゅ玉子 <myamyu@gmail.com>
2021-09-08 16:12:58 +0000

    error messageを修正

--- src/selenimum/errors.nim
+++ src/selenimum/errors.nim
@@ -1,4 +1,4 @@
-import httpclient
+import httpclient, json
 
 type
   SeleniumWebDriverException* = object of CatchableError
@@ -8,13 +8,18 @@ type
 
 proc makeErrorMessage(resp: Response): string =
   result = resp.status & "\n"
-  result.add(resp.body)
+  let obj = parseJson(resp.body)
+  result.add("  error: " & obj{"value", "error"}.getStr() & "\n")
+  result.add("  message: " & obj{"value", "message"}.getStr() & "\n")
 
 #[
   check error
 ]#
 proc checkHttpResponse*(resp: Response) =
   if resp.code == Http404:
+    let obj = parseJson(resp.body)
+    if obj{"value", "error"}.getStr() == "unknown command":
+      raise newException(SeleniumServerException, resp.makeErrorMessage())
     raise newException(SeleniumNotFoundException, "")
   if resp.code.is5xx or resp.code.is4xx:
     raise newException(SeleniumServerException, resp.makeErrorMessage())
