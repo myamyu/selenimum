2e40604b539d2c214218472d25a0856e6542f0c6
みゃみゅ玉子 <myamyu@gmail.com>
2021-09-23 17:04:31 +0000

    add tests

--- tests/test_element.nim
+++ tests/test_element.nim
@@ -13,6 +13,7 @@ suite "test element":
 
   test "find element":
     session.navigateTo(fmt"{testSiteOrigin}/")
+
     var elem: Element
 
     checkpoint("css selector")
@@ -33,6 +34,7 @@ suite "test element":
 
   test "find elements":
     session.navigateTo(fmt"{testSiteOrigin}/")
+
     var elems: seq[Element]
 
     checkpoint("css selector")
@@ -46,3 +48,63 @@ suite "test element":
     checkpoint("tag name")
     elems = session.findElementsByTagName("li")
     check(len(elems) == 3)
+
+  test "find element from element":
+    session.navigateTo(fmt"{testSiteOrigin}/")
+    let section = session.findElementByTagName("section")
+
+    var elem: Element
+
+    checkpoint("css selector")
+    elem = section.findElementBySelector("h2 + p")
+    check(elem.getText() == "This is section.")
+
+    checkpoint("ID")
+    elem = section.findElementById("sec1Title")
+    check(elem.getText() == "Test section 1")
+
+    checkpoint("xpath")
+    elem = section.findElementByXPath("//h2")
+    check(elem.getText() == "Test section 1")
+
+    checkpoint("tag name")
+    elem = section.findElementByTagName("p")
+    check(elem.getText() == "This is section.")
+
+  test "find elements from element":
+    session.navigateTo(fmt"{testSiteOrigin}/")
+    let ul = session.findElementByTagName("ul")
+
+    var elems: seq[Element]
+
+    checkpoint("css selector")
+    elems = ul.findElementsBySelector("li > span")
+    check(len(elems) == 2)
+
+    checkpoint("xpath")
+    elems = ul.findElementsByXPath("//li/span")
+    check(len(elems) == 2)
+
+    checkpoint("tag name")
+    elems = ul.findElementsByTagName("span")
+    check(len(elems) == 2)
+
+  test "click":
+    # TODO test
+    discard
+  
+  test "get tag name":
+    # TODO test
+    discard
+
+  test "set/clear value":
+    # TODO test
+    discard
+
+  test "get attribute value":
+    # TODO test
+    discard
+
+suite "test element error case":
+  # TODO test
+  discard
