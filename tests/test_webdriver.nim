d312398f20e8f7e0619f48e3611658e4ff0e15c7
みゃみゅ玉子 <myamyu@gmail.com>
2021-09-28 16:34:19 +0000

    update webdriver tests

--- tests/test_webdriver.nim
+++ tests/test_webdriver.nim
@@ -10,13 +10,6 @@ suite "test webdriver":
     check(status.ready)
 
   test "timeout":
-    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:4444/wd/hub", timeout=1)
+    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:4444/wd/hub", timeout=0)
     expect(TimeoutError):
       discard driver.status()
-
-  test "unknown endpoint":
-    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:8888/wd/hub")
-    try:
-      discard driver.status()
-    except OSError:
-      check(getCurrentExceptionMsg() == "Connection refused")
