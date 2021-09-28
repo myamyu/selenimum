865e45119ae9fdb0a77505b66ce224a678318dd2
みゃみゅ玉子 <myamyu@gmail.com>
2021-09-28 16:06:31 +0000

    add webdriver tests

---
+++ tests/test_webdriver.nim
@@ -0,0 +1,22 @@
+import unittest, net
+import selenimum
+
+suite "web driver":
+
+  test "status":
+    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:4444/wd/hub")
+    let status = driver.status()
+    echo $status
+    check(status.ready)
+
+  test "timeout":
+    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:4444/wd/hub", timeout=1)
+    expect(TimeoutError):
+      discard driver.status()
+
+  test "unknown endpoint":
+    let driver = newSeleniumWebDriver(baseUrl="http://selenium-hub:8888/wd/hub")
+    try:
+      discard driver.status()
+    except OSError:
+      check(getCurrentExceptionMsg() == "Connection refused")
